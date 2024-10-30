fa152058d4f69c94870b13828d5c8a98e51569fa
goodfy704 <goodfy704@gmail.com>
2024-10-30 22:05:31 +0200

    added tailwind

--- src/App.js
+++ src/App.js
@@ -3,11 +3,11 @@ import LikeButton from './components/like_button';
 
 function App() {
   return (
-    <div className="bg-dark-gray min-h-screen">
+    <div className="bg-black-russian min-h-screen">
       <div className="absolute inset-y-1/2 inset-x-1/2">
         <LikeButton />
       </div>
-    </div>
+    </div>  
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,42 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{html,js}'],
   theme: {
-    extend: {
-      screens: {
-        sm: '480px',
-        md: '768px',
-        lg: '976px',
-        xl: '1440px',
-      },
-      colors: {
-        'blue': '#1fb6ff',
-        'purple': '#7e5bef',
-        'pink': '#ff49db',
-        'orange': '#ff7849',
-        'green': '#13ce66',
-        'yellow': '#ffc82c',
-        'gray-dark': '#273444',
-        'gray': '#8492a6',
-        'gray-light': '#d3dce6',
-        'dark-gray': '#282c34',
-      },
-      fontFamily: {
-        sans: ['Graphik', 'sans-serif'],
-        serif: ['Merriweather', 'serif'],
-      },
-      extend: {
-        spacing: {
-          '128': '32rem',
-          '144': '36rem',
-        },
-        borderRadius: {
-          '4xl': '2rem',
-        }
-      }
+    colors: {
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+      'deep-charocal': '#2C2C2C',
+      'black-russian' : '#1c1c20',
     },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      spacing: {
+        '8xl': '96rem',
+        '9xl': '128rem',
+      },
+      borderRadius: {
+        '4xl': '2rem',
+      }
+    }
   },
-  plugins: [],
 }
+
