34b7962a164cb066c4ebfc65dfebbd6a0e157d3a
goodfy704 <goodfy704@gmail.com>
2025-01-07 01:44:41 +0200

    idgg-17 added not found page

--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,7 +1,11 @@
+import React from "react";
+
 function notFoundPage() {
-    return  <div>
-                <h1 className="text-yellow">Page not found</h1>
-            </div>
+    return(
+        <div className="">
+            <h1 className="text-white w-screen text-center h-screen content-center text-4xl">Seems like the summoner you are trying to search does not exist ;(</h1>
+        </div>
+    )
 }
 
 export default notFoundPage;
