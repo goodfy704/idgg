7fac0b5336f087cf4af2605cb955f31d4d8e57fe
goodfy704 <goodfy704@gmail.com>
2025-01-07 01:45:03 +0200

    idgg-18 added too many requests page

---
+++ src/components/tooManyRequests.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function tooManyRequests() {
+    return(
+        <div className="">
+            <h1 className="text-white w-screen text-center h-screen content-center text-4xl">Relaxing vibes only ;(</h1>
+        </div>
+    )
+}
+
+export default tooManyRequests;
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,15 @@ import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/home';
 import NotFoundPage from './components/NotFoundPage';
 import PlayerPage from './components/playerPage'
+import TooManyRequests from './components/tooManyRequests';
 
 function appRoutes() {
     return (
         <Router>
             <Routes>
                 <Route exact path="/" element={<Home />}/>
-                <Route exact path="/player" element={<NotFoundPage />}/>
+                <Route exact path="/notFound" element={<NotFoundPage />}/>
+                <Route exact path="/tooManyRequests" element={<TooManyRequests />}/>
                 <Route exact path="/:searchedPlayer" element={<PlayerPage />}/>
             </Routes>
         </Router>
